ae9173cb29e377ed72de05d0c4a69b1e7e167644
Doug <doug@hcsw.org>
2017-03-10 16:14:43 -0500

    back to head-only

--- logp.rb
+++ logp.rb
@@ -1,8 +1,7 @@
 class Logp < Formula
   desc "Command-line client for Log Periodic logging service"
   homepage "https://logperiodic.com"
-  url "https://github.com/hoytech/logp/archive/logp-0.1.2.tar.gz"
-  sha256 "e72b9b23437ea463921bb5088b4a01f33bd3667d1eb4630be749c3a9168058c5"
+  head "https://github.com/hoytech/logp.git", :tag => "logp-0.1.2"
 
   depends_on "openssl"
 
