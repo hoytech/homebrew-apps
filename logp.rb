a2b8957bf81aea877d035df8b73b25854c07a87a
Doug <doug@hcsw.org>
2017-03-22 08:23:10 -0400

    update to 0.1.3

--- logp.rb
+++ logp.rb
@@ -1,13 +1,11 @@
 class Logp < Formula
   desc "Command-line client for Log Periodic logging service"
   homepage "https://logperiodic.com"
-  head "https://github.com/hoytech/logp.git", :tag => "logp-0.1.2"
+  head "https://github.com/hoytech/logp.git", :tag => "logp-0.1.3"
 
   depends_on "openssl"
 
   def install
-    ## FIXME: This touch is a hack: the makefile looks for this file to see if the version has changed
-    system "touch", ".git/refs/heads/master"
     system "git", "submodule", "update", "--init"
     system "make", "XCXXFLAGS=-I/usr/local/opt/openssl/include", "XLDFLAGS=-L/usr/local/opt/openssl/lib", "-j4"
     bin.install "logp"
